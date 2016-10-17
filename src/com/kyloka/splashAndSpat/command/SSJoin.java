83e61df4da9df59de604320cdb671c8068f48437
BuildTools <unconfigured@null.spigotmc.org>
2016-10-16 20:38:02 -0400

    Testing the enumerators
    Also trying the exception

--- src/com/kyloka/splashAndSpat/command/SSJoin.java
+++ src/com/kyloka/splashAndSpat/command/SSJoin.java
@@ -1,9 +1,16 @@
 package com.kyloka.splashAndSpat.command;
 
+import com.kyloka.splashAndSpat.Main;
+import com.kyloka.splashAndSpat.exception.PlayerOnListException;
 import com.kyloka.splashAndSpat.game.GameList;
 import com.kyloka.splashAndSpat.game.GameState;
+import com.kyloka.splashAndSpat.player.PlayerUser;
+import org.bukkit.ChatColor;
 import org.bukkit.command.*;
 import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+
+import java.util.logging.Level;
 
 /**
  * Created by Matthew on 10/16/2016.
@@ -12,6 +19,11 @@ public class SSJoin implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         GameState derp = new GameState();
+        if(!(sender instanceof Player)){
+            sender.sendMessage(ChatColor.RED + "Cannot send this command through console");
+            return false;
+        }
+        Player player = (Player)sender;
         derp.setState(GameList.Waiting);
 
         if(derp.isState(GameList.Waiting)){
@@ -26,6 +38,16 @@ public class SSJoin implements CommandExecutor {
         if(derp.isState(GameList.END)){
             sender.sendMessage("The game ended");
         }
+        player.sendMessage("You");
+        PlayerUser test = new PlayerUser();
+        try{
+            test.removePlayer(player);
+        }
+        catch(PlayerOnListException e){
+            player.sendMessage("You are already in the game");
+            e.printStackTrace();
+        }
+
 
         return false;
     }
